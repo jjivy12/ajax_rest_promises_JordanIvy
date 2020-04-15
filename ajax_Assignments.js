344a7dd1d6570356ba5e8b1007b568a77fcd87b0
jjivy12 <jordan.ivy12@gmail.com>
2020-04-15 18:35:46 -0400

    HomeWork from freeCode Academy

---
+++ ajax_Assignments.js
@@ -0,0 +1,21 @@
+// link 1 answer below
+<script>
+  document.addEventListener('DOMContentLoaded', function(){
+    // Add your code below this line
+document.getElementById('getMessage').onclick= function(){}
+    // Add your code above this line
+  });
+</script>
+
+//Link 2 answer below 
+
+<script>
+  document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+      // Add your code below this line
+document.getElementsByClassName('message')[0].textContent="Here is the message";
+
+      // Add your code above this line
+    }
+  });
+</script>
